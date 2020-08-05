761292d0467c02bbb0b34edaa5615064ea50f962
Thomas Hoffmann <thomas.hoffmann@ecx.io>
2020-08-05 08:37:18 +0200

    added more tests

--- cypress/integration/percy-spec.js
+++ cypress/integration/percy-spec.js
@@ -1,7 +1,11 @@
 context('percy playground', () => {
-  describe('page with custom elements', () => {
-    it('can take snapshot', () => {
+  describe('page with custom form elements', () => {
+    it('can take snapshot with less form elements', () => {
       cy.visit('web/AEM Assets Asset Details.html');
+      cy.percySnapshot();
+    });
+    it('can take snapshot with many form elements', () => {
+      cy.visit('web/AEM Assets Asset Metadata.html');
       cy.percySnapshot();
     });
   });
