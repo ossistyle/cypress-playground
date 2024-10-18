e78d07f3ddccbe5b54467e731610aaa7b72432cd
Thomas Hoffmann <ossistyle@gmail.com>
2024-10-18 15:54:10 +0200

    WIP

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,37 +4,48 @@ import { defineConfig } from 'cypress';
 import cypressGrepPlugin from '@cypress/grep/src/plugin';
 import failFast from 'cypress-fail-fast/plugin';
 
-export default defineConfig({
-  e2e: {
-    baseUrl: null,
-    supportFile: 'cypress/support/index.{js,jsx,ts,tsx}',
-    experimentalRunAllSpecs: true,
-    testIsolation: true,
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      charts: true,
-      reportPageTitle: 'custom-title 4',
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveAllAttempts: true,
-    },
-    env: {
-      FAIL_FAST_STRATEGY: 'spec',
-      FAIL_FAST_ENABLED: true,
-      FAIL_FAST_BAIL: 0,
-      grepIntegrationFolder: './',
-      grepFilterSpecs: true,
-      grepOmitFiltered: true,
-    },
-    video: true,
-    screenshotOnRunFailure: true,
-    setupNodeEvents(on, config) {
-      cypressGrepPlugin(config);
-      failFast(on, config);
-      // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
-      require('cypress-mochawesome-reporter/plugin')(on);
-      return config;
-    },
+import defu from 'defu';
+import e2eDev from './cypress/configs/cypress.dev.config';
+
+const environments = [
+  {
+    dev: e2eDev,
   },
-});
+];
+const environment = environments[`${process.env.ENVIRONMENT}`];
+export default defineConfig(
+  defu(environment, {
+    e2e: {
+      baseUrl: null,
+      supportFile: 'cypress/support/index.{js,jsx,ts,tsx}',
+      experimentalRunAllSpecs: true,
+      testIsolation: true,
+      specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+      reporter: 'cypress-mochawesome-reporter',
+      reporterOptions: {
+        charts: true,
+        reportPageTitle: 'custom-title 4',
+        embeddedScreenshots: true,
+        inlineAssets: true,
+        saveAllAttempts: true,
+      },
+      env: {
+        FAIL_FAST_STRATEGY: 'spec',
+        FAIL_FAST_ENABLED: true,
+        FAIL_FAST_BAIL: 0,
+        grepIntegrationFolder: './',
+        grepFilterSpecs: true,
+        grepOmitFiltered: true,
+      },
+      video: true,
+      screenshotOnRunFailure: true,
+      setupNodeEvents(on, config) {
+        cypressGrepPlugin(config);
+        failFast(on, config);
+        // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
+        require('cypress-mochawesome-reporter/plugin')(on);
+        return config;
+      },
+    },
+  }),
+);
--- cypress/configs/cypress.dev.config.ts
+++ cypress/configs/cypress.dev.config.ts
@@ -1,15 +1,6 @@
-import { defineConfig } from 'cypress';
-import defu from 'defu';
-
-import defaultConfig from '../../cypress.config';
-
-export default defineConfig(
-  defu(
-    {
-      e2e: {
-        baseUrl: 'http://example.com',
-      },
-    },
-    defaultConfig,
-  ),
-);
+export default {
+  e2e: {
+    baseUrl: 'http://example.com',
+    ENVIRONMENT: '',
+  },
+};
