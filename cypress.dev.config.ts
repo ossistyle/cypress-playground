b082afb204e2da15dc848b97944600869a1c1cd9
Thomas 'ossistyle' Hoffmann <ossistyle@gmail.com>
2024-11-08 13:03:04 +0100

    feat: Move environment config to root

--- cypress.dev.config.ts
+++ cypress.dev.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'cypress';
 import defu from 'defu';
-
-import defaultConfig from '../../cypress.config';
+import defaultConfig from './cypress.config';
 
 export default defineConfig(
   defu(
     {
       e2e: {
         baseUrl: 'http://example.com',
+        excludeSpecPattern: [],
+      },
+      env: {
+        grepFilterSpecs: true,
+        grepOmitFiltered: true,
       },
     },
     defaultConfig,
